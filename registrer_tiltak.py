c51e2047cc389f17b28fceabc6697c817772ba9d
eventonsberg <even-kristian.tonsberg@ffi.no>
2026-09-25 15:11:03 +0200

    Oppdatert regneark-id for tiltaksregistrering

--- registrer_tiltak.py
+++ registrer_tiltak.py
@@ -8,18 +8,19 @@ from ulid import ULID
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 credentials = Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=scope)
 client = gspread.authorize(credentials)
+spreadsheet_id = "1FLDZ9ZibMww44XnBYChb-ecOTvD189TR-vZ6QpGREnM"
 
 def registrer_tiltak(data):
     unik_id = str(ULID())
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    sheet = client.open("Klimatiltak").sheet1
+    sheet = client.open_by_key(spreadsheet_id).sheet1
     sheet.append_row([unik_id, timestamp] + data)
     st.session_state['unik_id'] = unik_id
     st.cache_data.clear()
 
 @st.cache_data
 def hent_registrerte_tiltak():
-    sheet = client.open("Klimatiltak").sheet1
+    sheet = client.open_by_key(spreadsheet_id).sheet1
     records = sheet.get_all_records()
     df = pd.DataFrame(records)
     df.insert(0, 'Tiltaksnummer', range(1, len(df) + 1))
